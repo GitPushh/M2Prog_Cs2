da1ca74bab0abe185a09dfe909aaf77239cc85f8
Jaysen <39217@ma-web.nl>
2024-11-22 12:36:50 +0100

    Opdracht1 vraag

--- 01 functions/opracht01/Functions/Program.cs
+++ 01 functions/opracht01/Functions/Program.cs
@@ -20,7 +20,40 @@ namespace Functions
         internal void Run()
         {
             Console.WriteLine("Dit is nu de start van mijn programma");
-            Console.ReadLine();
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+            
+
+
+        }
+
+        internal void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+        }
+        internal void Vraag2()
+        {
+            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+            string antwoord = Console.ReadLine();
+        }
+        internal void Vraag3()
+        {
+            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+            string antwoord = Console.ReadLine();
+        }
+        internal void Vraag4()
+        {
+            Console.WriteLine("What would you do if you won a million dollars?");
+            string antwoord = Console.ReadLine();
+        }
+        internal void Vraag5()
+        {
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
+            string antwoord = Console.ReadLine();
         }
     }
 }
