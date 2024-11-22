5175a140c871b74481c3c8b3afe56ffaf0cab44a
Jaysen <39217@ma-web.nl>
2024-11-22 12:59:33 +0100

    Random

--- 01 functions/opracht01/Functions/Program.cs
+++ 01 functions/opracht01/Functions/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Deployment.Internal;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,18 @@ namespace Functions
 {
     internal class Program
     {
+        Random rnd = new Random();
+
+        string[] vragen = new string[]
+        {
+            "TestVraag",
+            "What is the worst game you played but that you liked anyway?",
+            "fffff",
+            "What is the worst game you played but that you liked anyway?fff",
+
+        };
+
+
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -19,12 +33,19 @@ namespace Functions
 
         internal void Run()
         {
+
+            string vraag0 = GetRandomVraag();
+            
+            Console.WriteLine(vraag0);  
+
             Console.WriteLine("Dit is nu de start van mijn programma");
             Vraag1();
             Vraag2();
             Vraag3();
             Vraag4();
             Vraag5();
+
+            
             
 
 
@@ -44,6 +65,7 @@ namespace Functions
         {
             Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
             string antwoord = Console.ReadLine();
+            
         }
         internal void Vraag4()
         {
@@ -54,6 +76,24 @@ namespace Functions
         {
             Console.WriteLine("What is the worst game you played but that you liked anyway?");
             string antwoord = Console.ReadLine();
+        }
+
+        internal string Vraag7()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();   
+
+            return antwoord;
+
+        }
+
+        
+
+        internal string GetRandomVraag()
+        {
+            int randomNumber = rnd.Next(1, 4);
+
+            return vragen[randomNumber];
         }
     }
 }
