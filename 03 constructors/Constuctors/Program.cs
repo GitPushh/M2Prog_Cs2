07605e056415e298cfb342f7e5dbe6e4392824d0
Jaysen <39217@ma-web.nl>
2024-12-13 11:27:25 +0100

    Update Program.cs

--- 03 constructors/Constuctors/Program.cs
+++ 03 constructors/Constuctors/Program.cs
@@ -8,22 +8,16 @@ namespace Constuctors
 {
     internal class Program
     {
-
-
-        
         static void Main(string[] args)
         {
             Program program = new Program();    
             program.Run();
             Console.Read();
-
         }
 
         internal void VoegVraagToeOpIndex(int index, string vraag, string antwoord)
         {
             vraag = "is kaas lekker?";
-            
-            
         }
 
 
@@ -32,7 +26,6 @@ namespace Constuctors
             QuizVraag quizvraag = new QuizVraag("hier komt e vraag", "hier komt het antwoord");
             Quiz quiz = new Quiz(10);
             quiz.VoegVraagToeOpIndex(0, quizvraag);
-            
         }
 
         
