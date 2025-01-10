cc0439fb61338f2252c0cec3170d07d993844294
Jaysen <39217@ma-web.nl>
2025-01-10 12:17:51 +0100

    02 lists

---
+++ 04 lists/Lists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();    
+            program.Run();
+        }
+
+        void Run()
+        {
+            string[] characters = { "Pac-Man", "Samus", "Crash bandicoot", "Spyro" };
+            List<string> characterlist = new List<string>();
+
+            characterlist.Add("Pac-man");
+
+            foreach (string character in characters)
+            {
+                characterlist.Add(character);
+            }
+
+            for (int i = 0; i < characterlist.Count; i++)
+            {
+                Console.WriteLine(characterlist[i]);
+            }
+
+            foreach (string character in characterlist)
+            {
+                Console.WriteLine(character);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
