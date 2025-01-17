33647320a9a6b7bc3f8f19407c85eec910162d10
Jaysen <39217@ma-web.nl>
2025-01-17 12:52:20 +0100

    05 dictionaries

--- dictionary/ConsoleApp1/Program.cs
+++ dictionary/ConsoleApp1/Program.cs
@@ -1,47 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
+        private Dictionary<string, Room> rooms = new Dictionary<string, Room>();
+
         static void Main(string[] args)
         {
-            Program program = new Program();    
+            Program program = new Program();
             program.Run();
-
-            
         }
 
         void Run()
         {
-            Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>();
-            personeelOpNummer.Add(38294, "jim");
-            personeelOpNummer.Add(789432, "tim");
-
-            string jim = personeelOpNummer[38294];
-            string tim = personeelOpNummer[789432];
-
-           
-
-            foreach(KeyValuePair<int, string> item in personeelOpNummer)
+            Room startRoom = new Room(0, 0, new string[]
             {
-                Console.WriteLine(item.Key + ": " + item.Value);
-            }
+                "xxxxx",
+                "x...x",
+                "x...x",
+                "x...x",
+                "xxxxx"
+            });
 
-            foreach(int key in personeelOpNummer.Keys)
+            Room room1 = new Room(0, 1, new string[]
             {
-                Console.WriteLine(key);
-            }
-            foreach(string value in personeelOpNummer.Values)
+                "xxxxx",
+                "x...x",
+                "x...x",
+                "x...x",
+                "xxxxx"
+            });
+            Room room2 = new Room(0, 2, new string[]
             {
-                Console.WriteLine(value);
-            }
+                "xxxxx",
+                "x...x",
+                "x...x",
+                "x...x",
+                "xxxxx"
+            });
+
+            rooms.Add(startRoom.GetRoomLocationKey(), startRoom);
+            rooms.Add(room1.GetRoomLocationKey(), room1);
+            rooms.Add(room2.GetRoomLocationKey(), room2);
+
+
+            ShowRoom(0, 0);
+            ShowRoom(0, 1);
+            ShowRoom(0, 2);
+            ShowRoom(0, 3);
+
+
+
 
             Console.ReadLine();
+        }
+
+         void ShowRoom(int x, int y)
+        {
+
+            bool hasRoom = rooms.ContainsKey($"{x}, {y}");
+            string key = $"{x},{y}";
+
+            if (rooms.ContainsKey(key) && hasRoom )
+            {
+                
+                    Room room = rooms[key];
+                    Console.WriteLine($"You are in room {x}, {y}");
+                    foreach (string r in room.level)
+                    {
+                        Console.WriteLine(r);
+                    }
+                
+            }else if (!hasRoom)
+            {
+                Console.WriteLine("Room does not exist");
+            }
+
+        }
+        
+    }
+
+    class Room
+    {
+        internal int x, y;
+        internal string[] level;
+
+        internal Room(int x, int y, string[] level)
+        {
+            this.x = x;
+            this.y = y;
+            this.level = level;
+        }
+        
+        internal string GetRoomLocationKey()
+        {
+            return $"{x},{y}";
         }
     }
 }
