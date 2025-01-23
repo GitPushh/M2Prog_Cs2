da1521987d314e076f3b57032136bf49e189f955
Jaysen <39217@ma-web.nl>
2025-01-23 14:20:06 +0100

    Read

---
+++ 06/MemSave/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MemSave
+{
+    internal class Program
+    {
+        string welkomstTekst = "hello world";
+        string saveFile = "welkomState.txt";
+
+        static void Main(string[] args)
+        {
+            Program program = new Program(); 
+            program.Run();  
+        }
+
+        void Run()
+        {
+            bool bestaatDeFile = File.Exists(saveFile);
+            if (bestaatDeFile)
+            {
+               welkomstTekst =  File.ReadAllText(saveFile);
+               Console.WriteLine(welkomstTekst);
+            }
+           
+            while (true) 
+            {
+
+                Console.WriteLine("Enter a text, then press enter");
+                File.WriteAllText(saveFile, welkomstTekst);
+                welkomstTekst = Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
