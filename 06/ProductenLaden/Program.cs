b4c0a6b785a2b927ef53a17e19e7824c1f0658be
Jaysen <39217@ma-web.nl>
2025-01-23 14:56:30 +0100

    dfea

---
+++ 06/ProductenLaden/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace ProductenLaden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        void Run()
+        {
+            string text = File.ReadAllText("Product.json");
+            string text2 = File.ReadAllText("products2.json");
+            Product product = JsonSerializer.Deserialize<Product>(text);    
+            Console.WriteLine(product.Name);
+            Console.WriteLine(product.Description); 
+            Console.WriteLine(product.Price);  
+            
+            Products2 products2 = JsonSerializer.Deserialize<Products2>(text2);
+            Console.WriteLine(products2.HP);
+            Console.WriteLine(products2.HP2);
+            Console.WriteLine(products2.HP3);
+
+
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+
+    public class Products2 
+    {
+        public string HP { get; set; }
+        public string HP2 { get; set; }
+        public string HP3 { get; set; }
+    }
+
+
+}
---
+++ 06/Schrijven/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Schrijven
+{
+    internal class Program
+    {
+        string[] kaas = { "feafea", "feafea", "kaasplank is lekker"};
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        void Run()
+        {
+
+            File.WriteAllLines("mijntekst.txt" ,kaas);
+
+        }
+    }
+
+    public class FavoriteGame 
+    {
+        public string Name;
+        public string Platform;
+    }
+
+}
